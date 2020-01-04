80b42ce3be49b554a430cdf9f006fdc256aa619c
braeden123 <braedensmith2@gmail.com>
2020-01-04 06:46:43 -0600

    fix char limit issues

--- cloud-function/index.js
+++ cloud-function/index.js
@@ -2,6 +2,7 @@ const Wiki = require('wikijs').default
 const wiki = Wiki()
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const regex = /wiki[: ]?["' ]?([A-Za-z0-9\s]+)["' ]?/gi
+const charLimit = 1500
 
 exports.fromTwilio = async (req, res) => {
     const twiml = new MessagingResponse();
@@ -11,9 +12,12 @@ exports.fromTwilio = async (req, res) => {
             const searchTerm = matches[1]
             const page = await wiki.find(searchTerm)
             const summary = await page.summary()
-            twiml.message(`${page.url()}:\n ${summary}`)
+            let output = `${page.url()}:\n ${summary}`
+            output = output.substring(0, output.length > charLimit ? charLimit : output.length)
+            twiml.message(output + (output.length == charLimit ? '...' : ''))
             console.log(`'${searchTerm}' | ${req.body.From} | ${page.url()}`)
         } else {
+            console.log(`Invalid syntax: '${req.body.Body}`)
             twiml.message('Syntax - wiki:"search term"')
         }
         res.writeHead(200, {
